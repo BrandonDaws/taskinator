402415da8f0c926b44fda90fbe601307a5efa968
velarde.brandon02@gmail.com <velarde.brandon02@gmail.com>
2022-03-24 18:02:09 -0500

    skipped lesson two, going back

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,12 @@
 var buttonE1 = document.querySelector("#save-task");
 var taskstoDoEl = document.querySelector("#tasks-to-do");
+var taskIdCounter = 0;
 
 var createTaskHandler = function(){
     var listItemEl = document.createElement("li");
     listItemEl.textContent = ("this is a new task")
     listItemEl.className = ("task-item");
+    listItemEl.setAttribute("data-task-id", taskIdCounter);
   taskstoDoEl.appendChild(listItemEl);
 
 }
